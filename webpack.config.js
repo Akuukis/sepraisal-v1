9ec8365681a38d9661d56b83698a0ec94268f164
Akuukis <akuukis@kalvis.lv>
2017-06-08 22:43:35 +0300

    add namedModulesPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -100,7 +100,8 @@ module.exports = {
     }),
     new HtmlWebpackPlugin({
       template: 'assets/index.html'
-    })
+    }),
+    new webpack.NamedModulesPlugin(),
   ],
   devServer: {
     contentBase: sourcePath,
