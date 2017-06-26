85c1ee87bc02ec5952b1d87415478a7556e5251c
Akuukis <akuukis@kalvis.lv>
2017-06-26 11:03:49 +0300

    accomodate for non-building ores in Material model

--- src/app/models/Material.ts
+++ src/app/models/Material.ts
@@ -1,14 +1,23 @@
+export interface MaterialCreationProps {
+  time: number,
+  prerequisites: { [title: string]: number, }
+}
+
 export class Material {
   type: string;
   subtype: string;
   weight: number;  // kg.
-  time: number;  // Seconds to build, baseBuildTime.
+  time?: number;  // Seconds to build, baseBuildTime.
+  prerequisites?: {
+    [title: string]: number,
+  }
 
-  constructor(type: string, subtype: string, weight: number, time: number) {
+  constructor(type: string, subtype: string, weight: number, creation?: MaterialCreationProps) {
     this.type = type;
     this.subtype = subtype;
     this.weight = weight;
-    this.time = time;
+    if(creation) this.time = creation.time;
+    if(creation) this.prerequisites = creation.prerequisites;
   }
 
   get title() { return `${this.type}/${this.subtype}`}
