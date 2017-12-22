3db46970809f8ecd0b3043e327dc7fc400b1ba6d
Akuukis <akuukis@kalvis.lv>
2017-12-22 17:12:16 +0200

    feat(selector): toggle selection, extract component

--- src/app/components/Selector/index.tsx
+++ src/app/components/Selector/index.tsx
@@ -9,21 +9,17 @@ import Paper from 'material-ui/Paper';
 import Typography from 'material-ui/Typography';
 import { Button } from 'material-ui';
 
-import {Component} from "../common/";
-import { STORE_BLUMMARY, STORE_ANALYSIS } from '../constants/stores';
-import { Blummary, BlummaryDTO } from '../models/Blummary';
-import { AnalysisStore, BlummaryStore } from '../stores/index';
+import {Component} from "../../common/";
+import { STORE_BLUMMARY, STORE_ANALYSIS } from '../../constants/stores';
+import { Blummary, BlummaryDTO } from '../../models/Blummary';
+import { AnalysisStore, BlummaryStore } from '../../stores/index';
 
-export type SelectorClasses = 'root'|'selected';
+import SelectorRow from './Row';
+
+export type SelectorClasses = 'root';
 const styles: StyleRulesCallback<SelectorClasses> = (theme) => ({
   root: {
     padding: '0.5em',
-  },
-  selected: {
-    background: theme.palette.primary[200],
-    '&:hover': {
-      background: theme.palette.primary[300],
-    }
   }
 })
 
@@ -35,14 +31,6 @@ export interface SelectorProps {
 class Selector extends Component<SelectorProps, SelectorClasses> {
   blummaryStore = this.props[STORE_BLUMMARY] as BlummaryStore;
   analysisStore = this.props[STORE_ANALYSIS] as AnalysisStore;
-
-  @computed get blummaries() {
-    return this.blummaryStore.map( (blummary)=>{ return {key: blummary.raw.title, title: blummary.raw.title}});
-  }
-
-  handleSelect = (event: React.MouseEvent<HTMLElement>) =>{
-    this.analysisStore.add(this.blummaryStore.get(event.currentTarget.getAttribute('value')));
-  }
 
   handleFile = async (event: React.ChangeEvent<HTMLInputElement>) => {
       event.preventDefault();
@@ -64,22 +52,9 @@ class Selector extends Component<SelectorProps, SelectorClasses> {
         <Typography type='headline'>Select a blueprint</Typography>
         <Grid container spacing={16}>
           <Grid item xs={12} sm={12} md={6}>
-            <Typography type='subheading'>Select an existing blueprint</Typography>
-            <List>
-              { this.blummaries.map<JSX.Element>( (line) => (
-                  <ListItem
-                    button
-                    key={line.key}
-                    value={line.key}
-                    className={computed(()=>this.analysisStore.has(line.key) ? this.props.classes.selected : '').get()}
-                    onClick={this.handleSelect}
-                  >
-                    <ListItemText
-                      primary={line.title}
-                    />
-                  </ListItem>
-                ))
-              }
+            <Typography type='subheading'>Default and uploaded blueprints</Typography>
+            <List dense>
+              { this.blummaryStore.map<JSX.Element>( (blummary) => <SelectorRow key={blummary.raw.title} blummary={blummary} /> ) }
             </List>
           </Grid>
           <Grid item xs={12} sm={12} md={6}>
